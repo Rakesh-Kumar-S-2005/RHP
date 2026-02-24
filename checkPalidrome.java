5ffa8daad1858804be9ef2513352efe7fa5d6d3e
Rakesh Kumar Sudhakar <230097.it@rmkec.ac.in>
2026-02-24 20:16:40 +0530

    Add palindrome checker in checkPalidrome.java

---
+++ checkPalidrome.java
@@ -0,0 +1,14 @@
+import java.util.*;
+
+class Main {
+    public static void main(String[] args) {
+        Scanner sc=new Scanner(System.in);
+        String str=sc.nextLine();
+        int flag=0;
+        for(char i :str.toCharArray())
+        {
+            flag=flag|(1<<(i-'a'));
+        }
+        System.out.println(flag==((1<<26)-1));
+    }
+}
